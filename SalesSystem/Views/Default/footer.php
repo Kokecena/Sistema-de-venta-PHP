6c90248c40958d06d434ff0dd9138e19bcdff111
AldoHernandezCruz <91111843+AldoHernandezCruz@users.noreply.github.com>
2021-09-23 16:48:15 -0500

    Add files via upload

---
+++ SalesSystem/Views/Default/footer.php
@@ -0,0 +1,9 @@
+        <footer class="border-top footer text-muted">
+            <div align='center'>
+                <p>&copy;PDHN</p>
+            </div>
+        </footer> 
+        <script src="<?php echo URL.RQ ?>js/jquery-3.6.0.min.js"></script>
+        <script src="<?php echo URL.RQ ?>js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
---
+++ SalesSystem/Views/Error/error.php
@@ -0,0 +1,4 @@
+<div class="container p-4">
+    <h1 class="text-danger">Error.</h1>
+    <h2 class="text-danger">Esta no es la palabra magica: <?php echo $model?></h2>
+</div>
